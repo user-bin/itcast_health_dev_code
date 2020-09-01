ddfa7abd653bbbc2ce0f168c0d903169013205d7
SUN <SUN@itcast.cn>
2020-09-01 16:48:42 +0800

    quartz 定时任务--入门案例

---
+++ health_job/src/main/java/com/itheima/job/ClearImageJob.java
@@ -0,0 +1,15 @@
+package com.itheima.job;
+
+import java.util.Date;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ * @Version 1.0
+ */
+public class ClearImageJob {
+
+    public void clear(){
+        System.out.println("执行了" + new Date());
+    }
+}
