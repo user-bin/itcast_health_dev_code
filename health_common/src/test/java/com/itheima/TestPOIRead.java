ccb78387de66b5f16ed123cf846d9ec224768acf
SUN <SUN@itcast.cn>
2020-09-02 09:59:56 +0800

    poi --读取excel【类型转换】

--- health_common/src/test/java/com/itheima/TestPOIRead.java
+++ health_common/src/test/java/com/itheima/TestPOIRead.java
@@ -1,11 +1,14 @@
 package com.itheima;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * @author 黑马程序员
@@ -34,7 +37,33 @@ public class TestPOIRead {
 //            System.out.println(lastCellNum);
             for (int j = 0; j < lastCellNum; j++) {
                 XSSFCell cell = row.getCell(j);
-                System.out.print(cell + "\t\t");
+                System.out.print(cell.toString() + "\t\t");
+                //获取单元格的类型
+                int cellType = cell.getCellType();
+                if(cellType == HSSFCell.CELL_TYPE_BLANK){
+                    System.out.println("空值");
+                }else if(cellType == HSSFCell.CELL_TYPE_BOOLEAN){
+                    System.out.println("布尔值");
+                    System.out.println(cell.getBooleanCellValue());
+                }else if(cellType == HSSFCell.CELL_TYPE_ERROR){
+                    System.out.println("错误");
+                }else if(cellType == HSSFCell.CELL_TYPE_FORMULA){
+                    System.out.println("公式");
+                    System.out.println(cell.getCellFormula());
+                }else if(cellType == HSSFCell.CELL_TYPE_NUMERIC){
+                    System.out.println("数值或者日期");
+                    if(HSSFDateUtil.isCellDateFormatted(cell)){
+                        Date dateCellValue = cell.getDateCellValue();
+                        System.out.println(dateCellValue);
+                    }else{
+                        //如果是一个数值类型， 会设置单元格类型为字符串，按照字符串读取这个数值
+                        cell.setCellType(HSSFCell.CELL_TYPE_STRING);
+                        System.out.println(cell.getStringCellValue());
+                    }
+                }else if(cellType == HSSFCell.CELL_TYPE_STRING){
+                    System.out.println(cell.getStringCellValue());
+                }
+
             }
             System.out.println();
         }
